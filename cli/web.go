37d68d55956712482a5fb1e550f625d378365afd
Jaayden Halko <jaayden.halko@gmail.com>
2025-03-06 11:35:37 +0000

    chore: add CORS handling

--- cli/web.go
+++ cli/web.go
@@ -80,6 +80,22 @@ func (r *RootCmd) WebsocketServer() *serpent.Command {
 			mux := chi.NewMux()
 
 			mux.Use(debugMiddleware(logger))
+			
+			// Add CORS middleware
+			mux.Use(func(next http.Handler) http.Handler {
+				return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					w.Header().Set("Access-Control-Allow-Origin", "*")
+					w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+					w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+					
+					if r.Method == "OPTIONS" {
+						w.WriteHeader(http.StatusOK)
+						return
+					}
+					
+					next.ServeHTTP(w, r)
+				})
+			})
 
 			mux.HandleFunc("/users/{dir}", func(rw http.ResponseWriter, r *http.Request) {
 				dirFS := os.DirFS(chi.URLParam(r, "dir"))
