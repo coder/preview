93aa0ac1057ae71d00c36958633cd84894cae3b6
McKayla Washburn <mckayla@hey.com>
2025-03-26 18:29:15 +0000

    note about ssh_private_key

--- types/owner.go
+++ types/owner.go
@@ -6,12 +6,14 @@ import (
 
 // Based on https://github.com/coder/terraform-provider-coder/blob/9a745586b23a9cb5de2f65a2dcac12e48b134ffa/provider/workspace_owner.go#L72
 type WorkspaceOwner struct {
-	ID              uuid.UUID                `json:"id"`
-	Name            string                   `json:"name"`
-	FullName        string                   `json:"full_name"`
-	Email           string                   `json:"email"`
-	SSHPublicKey    string                   `json:"ssh_public_key"`
-	SSHPrivateKey   string                   `json:"ssh_private_key"`
+	ID           uuid.UUID `json:"id"`
+	Name         string    `json:"name"`
+	FullName     string    `json:"full_name"`
+	Email        string    `json:"email"`
+	SSHPublicKey string    `json:"ssh_public_key"`
+	// SSHPrivateKey is intentionally omitted for now, due to the security risk
+	// that exposing it poses.
+	// SSHPrivateKey   string                   `json:"ssh_private_key"`
 	Groups          []string                 `json:"groups"`
 	SessionToken    string                   `json:"session_token"`
 	OIDCAccessToken string                   `json:"oidc_access_token"`
