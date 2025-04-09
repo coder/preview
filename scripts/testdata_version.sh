094bd50fd71cb6fc19cd51296d3553016c49e3aa
Steven Masley <stevenmasley@gmail.com>
2025-04-09 11:08:52 -0500

    script covers more cases

--- scripts/testdata_version.sh
+++ scripts/testdata_version.sh
@@ -16,17 +16,17 @@ VERSION_ARGUMENT=$1
 
 # Find and replace the string in all .tf files in the ./testdata/ directory
 find ./testdata/ -type f -name "*.tf" -exec sed -i.bak "/coder = {/{
+    # Capture the entire coder block
+    :a
     N
+    /}/!ba
     # Remove any existing version lines
     s|[ ]*version[ ]*=[^\\n]*\\n||g
-    # Add the new version line
-    s|    coder = {|    coder = {\n      version = \"$VERSION_ARGUMENT\"|
-    # Ensure the source line is correctly formatted
-    s|source[ ]*=[ ]*\"coder/coder\"|source = \"coder/coder\"|
-    s|source[ ]*\"coder/coder\"|source = \"coder/coder\"|
+    # Replace the coder block with the new format
+    s|    coder = {\n.*}|    coder = {\n      source = \"coder/coder\"\n      version = \"$VERSION_ARGUMENT\"\n    }|
 }" {} +
 
 # Remove backup files
 find ./testdata/ -type f -name "*.bak" -exec rm {} +
 
-echo "Replacement complete. Backup files have been removed."
+echo "Replacement complete."
