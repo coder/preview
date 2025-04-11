2a0e531069bfdf52765f8bef7e73418b8c5f2742
McKayla Washburn <mckayla@hey.com>
2025-04-11 16:29:04 +0000

    chore: remove additional secret-ish fields from `WorkspaceOwner`

--- types/owner.go
+++ types/owner.go
@@ -13,12 +13,16 @@ type WorkspaceOwner struct {
 	SSHPublicKey string    `json:"ssh_public_key"`
 	// SSHPrivateKey is intentionally omitted for now, due to the security risk
 	// that exposing it poses.
-	// SSHPrivateKey   string                   `json:"ssh_private_key"`
-	Groups          []string                 `json:"groups"`
-	SessionToken    string                   `json:"session_token"`
-	OIDCAccessToken string                   `json:"oidc_access_token"`
-	LoginType       string                   `json:"login_type"`
-	RBACRoles       []WorkspaceOwnerRBACRole `json:"rbac_roles"`
+	// SSHPrivateKey string `json:"ssh_private_key"`
+	Groups []string `json:"groups"`
+	// SessionToken is intentionally omitted for now, due to the security risk
+	// that exposing it poses.
+	// SessionToken string `json:"session_token"`
+	// OIDCAccessToken is intentionally omitted for now, due to the security risk
+	// that exposing it poses.
+	// OIDCAccessToken string `json:"oidc_access_token"`
+	LoginType string                   `json:"login_type"`
+	RBACRoles []WorkspaceOwnerRBACRole `json:"rbac_roles"`
 }
 
 type WorkspaceOwnerRBACRole struct {
