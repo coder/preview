587c3d6b22a5f3af293adb10b4c72ad7aee0e7d0
Steven Masley <stevenmasley@gmail.com>
2025-11-17 08:11:53 -0600

    remove json serialization of cty types

--- types/terraform.go
+++ types/terraform.go
@@ -6,9 +6,11 @@ import (
 
 type Variable struct {
 	Name string `json:"name"`
-	// Unsure how cty values json marshal
-	Default     cty.Value `json:"default"`
-	Type        cty.Type  `json:"type"`
+	// JSON marshalling of cty types and values is not safe. Until these json values
+	// are needed, serialization of them is not supported.
+	// If serialization is supported, a custom marshaller will be needed.
+	Default     cty.Value `json:"-"`
+	Type        cty.Type  `json:"-"`
 	Description string    `json:"description"`
 	Nullable    bool      `json:"nullable"`
 	Sensitive   bool      `json:"sensitive"`
